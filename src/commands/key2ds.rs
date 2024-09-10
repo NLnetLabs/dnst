119a6e5c41eed1869064f9998257297be9b7cbae
Terts Diepraam <terts.diepraam@gmail.com>
2024-09-10 14:16:53 +0200

    key2ds: better errror handling and --algorithm

--- src/commands/key2ds.rs
+++ src/commands/key2ds.rs
@@ -1,6 +1,6 @@
 use std::{fs::File, io::Write, path::PathBuf};
 
-use clap::Parser;
+use clap::{builder::ValueParser, Parser};
 use domain::{
     base::{
         iana::{DigestAlg, SecAlg},
@@ -25,29 +25,62 @@ pub struct Key2ds {
     write_to_stdout: bool,
 
     /// use SHA1 for the DS hash
-    #[arg(short = '1', overrides_with_all = ["one", "two", "four"])]
+    #[arg(short = '1', overrides_with_all = ["one", "two", "four", "algorithm"])]
     one: bool,
 
     /// use SHA256 for the DS hash
-    #[arg(short = '2', overrides_with_all = ["one", "two", "four"])]
+    #[arg(short = '2', overrides_with_all = ["one", "two", "four", "algorithm"])]
     two: bool,
 
     /// use SHA384 for the DS hash
-    #[arg(short = '4', overrides_with_all = ["one", "two", "four"])]
+    #[arg(short = '4', overrides_with_all = ["one", "two", "four", "algorithm"])]
     four: bool,
+
+    /// algorithm to use for digest
+    #[arg(
+        short = 'a',
+        long = "algorithm",
+        overrides_with_all = ["one", "two", "four", "algorithm"],
+        value_parser = ValueParser::new(parse_digest_alg)
+    )]
+    algorithm: Option<DigestAlg>,
 
     /// Keyfile to read
     #[arg()]
     keyfile: PathBuf,
 }
 
+pub fn parse_digest_alg(arg: &str) -> Result<DigestAlg, Error> {
+    if let Ok(num) = arg.parse() {
+        let alg = DigestAlg::from_int(num);
+        if alg.to_mnemonic().is_some() {
+            Ok(alg)
+        } else {
+            Err(Error::from("unknown algorithm number"))
+        }
+    } else {
+        DigestAlg::from_mnemonic(arg.as_bytes()).ok_or(Error::from("unknown algorithm mnemonic"))
+    }
+}
+
 impl Key2ds {
     pub fn execute(self) -> Result<(), Error> {
-        let mut file = std::fs::File::open(&self.keyfile).unwrap();
+        let mut file = std::fs::File::open(&self.keyfile).map_err(|e| {
+            format!(
+                "Failed to open public key file \"{}\": {e}",
+                self.keyfile.display()
+            )
+        })?;
         let zonefile = domain::zonefile::inplace::Zonefile::load(&mut file).unwrap();
         for entry in zonefile {
-            let entry = entry.unwrap();
+            let entry = entry.map_err(|e| {
+                format!(
+                    "Error while reading public key file \"{}\": {e}",
+                    self.keyfile.display()
+                )
+            })?;
 
+            // We only care about records in a zonefile
             let Entry::Record(record) = entry else {
                 continue;
             };
@@ -56,6 +89,7 @@ impl Key2ds {
             let ttl = record.ttl();
             let owner = record.owner();
 
+            // Of the records that we see, we only care about DNSKEY records
             let ScannedRecordData::Dnskey(dnskey) = record.data() else {
                 continue;
             };
@@ -70,9 +104,17 @@ impl Key2ds {
             let sec_alg = dnskey.algorithm();
             let digest_alg = self.determine_hash(sec_alg);
 
-            let digest = dnskey.digest(&owner, digest_alg).unwrap();
+            if digest_alg == DigestAlg::GOST {
+                return Err("Error: the GOST algorithm is deprecated and must not be used. Try a different algorithm.".into());
+            }
 
-            let ds = Ds::new(key_tag, sec_alg, digest_alg, digest).unwrap();
+            let digest = dnskey
+                .digest(&owner, digest_alg)
+                .map_err(|e| format!("Error computing digest: {e}"))?;
+
+            let ds = Ds::new(key_tag, sec_alg, digest_alg, digest).expect(
+                "Infallible because the digest won't be too long since it's a valid digest",
+            );
 
             let rr = Record::new(owner, class, ttl, ds);
 
@@ -81,9 +123,11 @@ impl Key2ds {
             } else {
                 let owner = owner.fmt_with_dot();
                 let sec_alg = sec_alg.to_int();
-                let mut out_file =
-                    File::create(format!("K{owner}+{sec_alg:03}+{key_tag:05}.ds")).unwrap();
-                writeln!(out_file, "{rr}").unwrap();
+                let filename = format!("K{owner}+{sec_alg:03}+{key_tag:05}.ds");
+                let mut out_file = File::create(&filename)
+                    .map_err(|e| format!("Could not create file \"{filename}\": {e}"))?;
+                writeln!(out_file, "{rr}")
+                    .map_err(|e| format!("Could not write to file \"{filename}\": {e}"))?;
             }
         }
 
@@ -98,6 +142,8 @@ impl Key2ds {
             return DigestAlg::SHA256;
         } else if self.four {
             return DigestAlg::SHA384;
+        } else if let Some(a) = self.algorithm {
+            return a;
         }
 
         // Otherwise we try to determine a similar hash to the key
