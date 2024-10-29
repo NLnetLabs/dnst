5746c3b0c90d176c2f5ef3539d4dcc0627f59aff
arya dradjica <arya@nlnetlabs.nl>
2024-10-29 16:08:23 +0100

    [keygen] Add help documentation

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -9,7 +9,29 @@ use super::error::Error;
 #[derive(Clone, Debug, clap::Subcommand)]
 pub enum Command {
     /// Generate a new key pair for a given domain name
-    #[command(name = "keygen")]
+    ///
+    /// The following files will be created:
+    ///
+    /// - K<name>+<alg>+<tag>.key: The public key file
+    ///
+    ///   This is a DNSKEY resource record in zone file format.
+    ///
+    /// - K<name>+<alg>+<tag>.private: The private key file
+    ///
+    ///   This is a text file in the conventional BIND format which
+    ///   contains fields describing the private key data.
+    ///
+    /// - K<name>+<alg>+<tag>.ds: The public key digest file
+    ///
+    ///   This is a DS resource record in zone file format.
+    ///   It is only created for key signing keys.
+    ///
+    /// <name> is the fully-qualified owner name for the key (with a trailing dot).
+    /// <alg> is the algorithm number of the key, zero-padded to 3 digits.
+    /// <tag> is the 16-bit tag of the key, zero-padded to 5 digits.
+    ///
+    /// Upon completion, 'K<name>+<alg>+<tag>' will be printed.
+    #[command(name = "keygen", verbatim_doc_comment)]
     Keygen(self::keygen::Keygen),
 
     /// Print the NSEC3 hash of a given domain name
