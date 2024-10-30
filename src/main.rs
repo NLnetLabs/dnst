f5e644a3cd46eff5e6e6bd3bef58e07759588ac4
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2024-10-30 23:52:05 +0100

    Revert exit code change as this has been moved into PR #13.

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,7 @@
 use clap::Parser;
 
 fn main() {
-    // Ensure that we exit with either 0 or 1, to match ldns-xxx command
-    // behaviour. We can't just call Args::parse() because Clap will exit with
-    // code 2 on argument parsing error, so instead call try_parse() and
-    // handle it ourselves.
-    match dnst::Args::try_parse() {
-        Ok(args) => {
-            if let Err(err) = args.execute() {
-                eprintln!("{}", err);
-            } else {
-                std::process::exit(0);
-            }
-        }
-
-        Err(err) => {
-            // Ensure we benefit from Clap pretty coloured error output.
-            let _ = err.print();
-        }
+    if let Err(err) = dnst::Args::parse().execute() {
+        eprintln!("{}", err);
     }
-
-    std::process::exit(1);
 }
