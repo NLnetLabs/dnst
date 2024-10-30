8b344d7c09388ed8468fac4fae2c76a199c83037
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2024-10-30 22:40:16 +0100

    Exit with code 1 on argument parsing error, not 2 as Clap normally does, and handle being invoked via an ldns tool name symlink.

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -8,7 +8,7 @@ use super::error::Error;
 #[derive(Clone, Debug, clap::Subcommand)]
 pub enum Command {
     /// Print the NSEC3 hash of a given domain name
-    #[command(name = "nsec3-hash")]
+    #[command(name = "nsec3-hash", alias = "ldns-nsec3-hash")]
     Nsec3Hash(self::nsec3hash::Nsec3Hash),
 
     /// Show the manual pages
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,42 @@
-use clap::Parser;
+use clap::{command, Args, FromArgMatches};
 
 fn main() {
-    if let Err(err) = dnst::Args::parse().execute() {
-        eprintln!("{}", err);
+    // Ensure that we exit with either 0 or 1, to match ldns-xxx command
+    // behaviour. We can't just call Args::parse() because Clap will exit with
+    // code 2 on argument parsing error, so instead call try_parse() and
+    // handle it ourselves.
+    let cli = dnst::Args::augment_args(command!());
+
+    // Try with multicall first.
+    let res = cli
+        .clone()
+        .multicall(true)
+        .try_get_matches()
+        .and_then(|m| dnst::commands::Command::from_arg_matches(&m))
+        .or_else(|err| {
+            if err.kind() == clap::error::ErrorKind::InvalidSubcommand {
+                // Try without multicall.
+                let matches = cli.get_matches();
+                dnst::commands::Command::from_arg_matches(&matches)
+            } else {
+                Err(err)
+            }
+        });
+
+    match res {
+        Ok(cmd) => {
+            if let Err(err) = cmd.execute() {
+                eprintln!("{}", err);
+            } else {
+                std::process::exit(0);
+            }
+        }
+
+        Err(err) => {
+            // Ensure we benefit from Clap pretty coloured error output.
+            let _ = err.print();
+        }
     }
+
+    std::process::exit(1);
 }
