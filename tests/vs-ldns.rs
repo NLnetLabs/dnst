e62938853ec3b9d2d1eab37f3b6e215d0408fa52
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2024-11-06 22:22:53 +0100

    Extend the integration test to compare exit codes, and also to give a more human readable error message on test failure.

--- tests/vs-ldns.rs
+++ tests/vs-ldns.rs
@@ -19,6 +19,8 @@ fn nsec3_hash() {
             &["-t", &iterations.to_string(), "example.com"],
         );
     }
+
+    assert_exit_code_ldns_eq_dnst("ldns-nsec3-hash", "nsec3-hash", &[]);
 }
 
 fn assert_ldns_eq_dnst(ldns_command: &str, dnst_subcommand: &str, args: &[&str]) {
@@ -30,15 +32,60 @@ fn assert_ldns_eq_dnst(ldns_command: &str, dnst_subcommand: &str, args: &[&str])
         .output()
         .unwrap();
 
-    assert_eq!(std::str::from_utf8(&ldns_output.stderr), Ok(""));
-    assert_eq!(std::str::from_utf8(&dnst_output.stderr), Ok(""));
-    assert!(!ldns_output.stdout.is_empty());
-    assert!(!dnst_output.stdout.is_empty());
+    assert_eq!(
+        std::str::from_utf8(&ldns_output.stderr),
+        Ok(""),
+        "Unexpected stderr content for command: {ldns_command} {}",
+        args.join(" ")
+    );
+    assert_eq!(
+        std::str::from_utf8(&dnst_output.stderr),
+        Ok(""),
+        "Unexpected stderr content for command: {dnst_subcommand} {}",
+        args.join(" ")
+    );
+    assert!(
+        !ldns_output.stdout.is_empty(),
+        "Unexpected stdout content for command: {ldns_command} {}: {:?}",
+        args.join(" "),
+        std::str::from_utf8(&ldns_output.stdout)
+    );
+    assert!(
+        !dnst_output.stdout.is_empty(),
+        "Unexpected stdout content for command: {dnst_subcommand} {}: {:?}",
+        args.join(" "),
+        std::str::from_utf8(&dnst_output.stdout)
+    );
+    assert_eq!(
+        ldns_output.status.code(),
+        dnst_output.status.code(),
+        "Exit code mismatch for command: {ldns_command}/{dnst_subcommand} {}",
+        args.join(" ")
+    );
 
     // This will only work for LDNS commands whose output we are able to
     // replicate exactly.
     assert_eq!(
         std::str::from_utf8(&ldns_output.stdout),
-        std::str::from_utf8(&dnst_output.stdout)
+        std::str::from_utf8(&dnst_output.stdout),
+        "Stdout content mismatch for command: {ldns_command}/{dnst_subcommand} {}",
+        args.join(" ")
+    );
+}
+
+fn assert_exit_code_ldns_eq_dnst(ldns_command: &str, dnst_subcommand: &str, args: &[&str]) {
+    let ldns_output = Command::new(ldns_command).args(args).output().unwrap();
+
+    let dnst_output = test_bin::get_test_bin("dnst")
+        .arg(dnst_subcommand)
+        .args(args)
+        .output()
+        .unwrap();
+
+    assert_eq!(
+        ldns_output.status.code(),
+        dnst_output.status.code(),
+        "Exit code mismatch for command: {ldns_command}/{dnst_subcommand} {}",
+        args.join(" ")
     );
 }
