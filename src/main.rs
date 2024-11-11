0349b10aac7fbc4a1e8389007db52dee937df585
Terts Diepraam <terts.diepraam@gmail.com>
2024-11-11 13:41:13 +0100

    use clap default exit code of 2 for dnst versions of commands

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use dnst::commands::{nsec3hash::Nsec3Hash, LdnsCommand};
 fn main() {
     // If none of the ldns-* tools matched, then we continue with clap
     // argument parsing.
-    let args = try_ldns_compatibility().unwrap_or_else(parse_clap);
+    let args = try_ldns_compatibility().unwrap_or_else(dnst::Args::parse);
 
     if let Err(err) = args.execute() {
         eprintln!("{}", err);
@@ -28,19 +28,6 @@ fn try_ldns_compatibility() -> Option<dnst::Args> {
         Err(e) => {
             eprintln!("{e}");
             std::process::exit(1)
-        }
-    }
-}
-
-fn parse_clap() -> dnst::Args {
-    let res = dnst::Args::try_parse();
-    match res {
-        Ok(args) => args,
-        Err(e) => {
-            // Clap normally has an exit code of 2, but we want an
-            // exit code of 1.
-            e.print().unwrap();
-            std::process::exit(1);
         }
     }
 }
