19e95a52db4d711b72f3de01b6e1ad0baafe4c13
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2024-11-12 15:22:41 +0100

    Don't test with sym link in integration test, do that later in Ploutos packaging.

--- tests/vs-ldns.rs
+++ tests/vs-ldns.rs
@@ -1,101 +1,116 @@
-#![cfg(unix)]
 use std::process::Command;
-use tempfile::TempDir;
+
+const TEST_ZONE_NAME: &str = "nlnetlabs.nl";
+const LDNS_NSEC3_CMD: &str = "ldns-nsec3-hash";
+const DNST_NSEC3_SUBCMD: &str = "nsec3-hash";
 
 #[ignore = "should only be run if ldns command line tools are installed"]
 #[test]
 fn nsec3_hash() {
-    assert_ldns_eq_dnst("ldns-nsec3-hash", &["example.com"]);
-    assert_ldns_eq_dnst("ldns-nsec3-hash", &["-a", "1", "example.com"]);
-    assert_ldns_eq_dnst("ldns-nsec3-hash", &["-s", "", "example.com"]);
-    assert_ldns_eq_dnst("ldns-nsec3-hash", &["-s", "DEADBEEF", "example.com"]);
+    // Note: ldns-nsec3-hash defaults NSEC3 iterations to 1, while dnst
+    // nsec-hash defaults NSEC3 iterations to 0.
+    assert_cmds_eq(
+        &[LDNS_NSEC3_CMD, TEST_ZONE_NAME],
+        &[DNST_NSEC3_SUBCMD, "--iterations", "1", TEST_ZONE_NAME],
+    );
+    assert_cmds_eq(
+        &[LDNS_NSEC3_CMD, TEST_ZONE_NAME, "-t", "0"],
+        &[DNST_NSEC3_SUBCMD, TEST_ZONE_NAME],
+    );
+    assert_cmds_eq(
+        &[LDNS_NSEC3_CMD, "-a", "1", TEST_ZONE_NAME],
+        &[
+            DNST_NSEC3_SUBCMD,
+            "--iterations",
+            "1",
+            "--algorithm",
+            "1",
+            TEST_ZONE_NAME,
+        ],
+    );
+    assert_cmds_eq(
+        &[LDNS_NSEC3_CMD, "-s", "", TEST_ZONE_NAME],
+        &[
+            DNST_NSEC3_SUBCMD,
+            "--iterations",
+            "1",
+            "--salt",
+            "",
+            TEST_ZONE_NAME,
+        ],
+    );
+    assert_cmds_eq(
+        &[LDNS_NSEC3_CMD, "-s", "DEADBEEF", TEST_ZONE_NAME],
+        &[
+            DNST_NSEC3_SUBCMD,
+            "--iterations",
+            "1",
+            "--salt",
+            "DEADBEEF",
+            TEST_ZONE_NAME,
+        ],
+    );
 
     for iterations in 0..10 {
-        assert_ldns_eq_dnst(
-            "ldns-nsec3-hash",
-            &["-t", &iterations.to_string(), "example.com"],
+        assert_cmds_eq(
+            &[
+                LDNS_NSEC3_CMD,
+                "-t",
+                &iterations.to_string(),
+                TEST_ZONE_NAME,
+            ],
+            &[
+                DNST_NSEC3_SUBCMD,
+                "-i",
+                &iterations.to_string(),
+                TEST_ZONE_NAME,
+            ],
         );
     }
-
-    assert_exit_code_ldns_eq_dnst("ldns-nsec3-hash", &[]);
 }
 
-fn create_temp_symlink(ldns_command: &str) -> std::path::PathBuf {
-    let bin_cmd = test_bin::get_test_bin("dnst");
-    let bin_path = bin_cmd.get_program();
-    let link_path = TempDir::new()
-        .expect("Failed to create temporary directory")
-        .into_path()
-        .join(ldns_command);
-    std::os::unix::fs::symlink(bin_path, link_path.clone()).unwrap_or_else(|_| {
-        panic!(
-            "Failed to create symbolic link {} -> {}",
-            link_path.display(),
-            bin_path.to_str().unwrap(),
-        )
-    });
-    link_path
-}
+fn assert_cmds_eq(cmd1: &[&str], cmd2: &[&str]) {
+    let cmd1_output = Command::new(&cmd1[0]).args(&cmd1[1..]).output().unwrap();
 
-fn assert_ldns_eq_dnst(ldns_command: &str, args: &[&str]) {
-    let ldns_output = Command::new(ldns_command).args(args).output().unwrap();
-
-    let dnst_as_ldns_bin = create_temp_symlink(ldns_command);
-
-    let dnst_output = Command::new(dnst_as_ldns_bin).args(args).output().unwrap();
+    let cmd2_output = test_bin::get_test_bin("dnst").args(cmd2).output().unwrap();
 
     assert_eq!(
-        std::str::from_utf8(&ldns_output.stderr),
+        std::str::from_utf8(&cmd1_output.stderr),
         Ok(""),
-        "Unexpected stderr content for ldns command: {ldns_command} {}",
-        args.join(" ")
+        "Unexpected stderr content for command: {}",
+        cmd1.join(" ")
     );
     assert_eq!(
-        std::str::from_utf8(&dnst_output.stderr),
+        std::str::from_utf8(&cmd2_output.stderr),
         Ok(""),
-        "Unexpected stderr content for dnst command: {ldns_command} {}",
-        args.join(" ")
+        "Unexpected stderr content for command: {}",
+        cmd2.join(" ")
     );
     assert!(
-        !ldns_output.stdout.is_empty(),
-        "Unexpected stdout content for ldns command: {ldns_command} {}: {:?}",
-        args.join(" "),
-        std::str::from_utf8(&ldns_output.stdout)
+        !cmd1_output.stdout.is_empty(),
+        "Unexpected stdout content for ldns command: {}: {:?}",
+        cmd1.join(" "),
+        std::str::from_utf8(&cmd1_output.stdout)
     );
     assert!(
-        !dnst_output.stdout.is_empty(),
-        "Unexpected stdout content for dnst command: {ldns_command} {}: {:?}",
-        args.join(" "),
-        std::str::from_utf8(&dnst_output.stdout)
+        !cmd2_output.stdout.is_empty(),
+        "Unexpected stdout content for dnst command: {}: {:?}",
+        cmd2.join(" "),
+        std::str::from_utf8(&cmd2_output.stdout)
     );
     assert_eq!(
-        ldns_output.status.code(),
-        dnst_output.status.code(),
-        "Exit code mismatch for command: {ldns_command} {}",
-        args.join(" ")
+        cmd1_output.status.code(),
+        cmd2_output.status.code(),
+        "Exit code mismatch for command: {}",
+        cmd1.join(" ")
     );
 
     // This will only work for LDNS commands whose output we are able to
     // replicate exactly.
     assert_eq!(
-        std::str::from_utf8(&ldns_output.stdout),
-        std::str::from_utf8(&dnst_output.stdout),
-        "Stdout content mismatch for command: {ldns_command} {}",
-        args.join(" ")
-    );
-}
-
-fn assert_exit_code_ldns_eq_dnst(ldns_command: &str, args: &[&str]) {
-    let ldns_output = Command::new(ldns_command).args(args).output().unwrap();
-
-    let dnst_as_ldns_bin = create_temp_symlink(ldns_command);
-
-    let dnst_output = Command::new(dnst_as_ldns_bin).args(args).output().unwrap();
-
-    assert_eq!(
-        ldns_output.status.code(),
-        dnst_output.status.code(),
-        "Exit code mismatch for command: {ldns_command} {}",
-        args.join(" ")
+        std::str::from_utf8(&cmd1_output.stdout),
+        std::str::from_utf8(&cmd2_output.stdout),
+        "Stdout content mismatch for command: {}",
+        cmd1.join(" ")
     );
 }
