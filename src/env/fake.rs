50e1dc4f3fae0bfd2933e9945768c3a994fb142d
Terts Diepraam <terts.diepraam@gmail.com>
2024-11-15 10:44:25 +0100

    document some more of the testing utilities

--- src/env/fake.rs
+++ src/env/fake.rs
@@ -7,6 +7,10 @@ use crate::{error::Error, parse_args, run, Args};
 
 use super::Env;
 
+/// A command to run in a [`FakeEnv`]
+///
+/// This is used for testing the utilities, running the real code in a fake
+/// environment.
 #[derive(Clone)]
 pub struct FakeCmd {
     /// The (sub)command to run, including `argv[0]`
@@ -16,23 +20,27 @@ pub struct FakeCmd {
     pub args: Vec<OsString>,
 }
 
+/// The result of running a [`FakeCmd`]
+///
+/// The fields are public to allow for easy assertions in tests.
 pub struct FakeResult {
     pub exit_code: u8,
     pub stdout: String,
     pub stderr: String,
 }
 
-/// Use fake I/O and Stelline for testing
+/// An environment that mocks interaction with the outside world
 pub struct FakeEnv {
-    // pub stelline: Option<Stelline>,
-    // pub curr_step_value: Option<Arc<CurrStepValue>>,
+    /// Description of the command being run
     pub cmd: FakeCmd,
 
-    /// The collected stdout
+    /// The mocked stdout
     pub stdout: FakeStream,
 
-    /// The collected stderr
+    /// The mocked stderr
     pub stderr: FakeStream,
+    // pub stelline: Option<Stelline>,
+    // pub curr_step_value: Option<Arc<CurrStepValue>>,
 }
 
 impl Env for FakeEnv {
@@ -54,6 +62,9 @@ impl Env for FakeEnv {
 }
 
 impl FakeCmd {
+    /// Construct a new [`FakeCmd`] with a given command.
+    ///
+    /// The command can consist of multiple strings to specify a subcommand.
     pub fn new<S: Into<OsString>>(cmd: impl IntoIterator<Item = S>) -> Self {
         Self {
             cmd: cmd.into_iter().map(Into::into).collect(),
@@ -61,13 +72,21 @@ impl FakeCmd {
         }
     }
 
+    /// Add arguments to a clone of the [`FakeCmd`]
+    ///
+    /// ```rust,ignore
+    /// let cmd = FakeCmd::new(["dnst"])
+    /// let sub1 = cmd.args(["sub1"]);  // dnst sub1
+    /// let sub2 = cmd.args(["sub2"]);  // dnst sub2
+    /// let sub3 = sub2.args(["sub3"]); // dnst sub2 sub3
+    /// ```
     pub fn args<S: Into<OsString>>(&self, args: impl IntoIterator<Item = S>) -> Self {
-        Self {
-            args: args.into_iter().map(Into::into).collect(),
-            ..self.clone()
-        }
+        let mut new = self.clone();
+        new.args.extend(args.into_iter().map(Into::into));
+        new
     }
 
+    /// Parse the arguments of this [`FakeCmd`] and return the result
     pub fn parse(&self) -> Result<Args, Error> {
         let env = FakeEnv {
             cmd: self.clone(),
@@ -77,6 +96,7 @@ impl FakeCmd {
         parse_args(env)
     }
 
+    /// Run the [`FakeCmd`] in a [`FakeEnv`], returning a [`FakeResult`]
     pub fn run(&self) -> FakeResult {
         let env = FakeEnv {
             cmd: self.clone(),
@@ -106,6 +126,7 @@ impl FakeEnv {
     }
 }
 
+/// A type to used to mock stdout and stderr
 #[derive(Clone, Default)]
 pub struct FakeStream(Arc<RefCell<String>>);
 
