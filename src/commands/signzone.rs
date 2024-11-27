b0b4c10a3149ff19ac4f6158535dfbcbdb4becb9
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2024-11-27 08:56:31 +0100

    Remove incorrect formatting logic.

--- src/commands/signzone.rs
+++ src/commands/signzone.rs
@@ -743,7 +743,7 @@ impl SignZone {
                     let ZoneRecordData::Rrsig(rrsig) = covering_rrsig_rr.data() else {
                         unreachable!();
                     };
-                    if self.extra_comments && rrsig.type_covered() == Rtype::NSEC3 {
+                    if self.extra_comments {
                         writer.write_str(";\n")?;
                     }
                 }
