ff28d64aba37e7bea3ed5bbba4d302db25b21198
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2025-05-16 12:25:13 +0200

    Also do sanity checks post-upgrade.

--- pkg/test-scripts/test-dnst.sh
+++ pkg/test-scripts/test-dnst.sh
@@ -7,11 +7,17 @@ case $1 in
   post-install)
     # Run some sanity checks
     dnst --version
+    ldns-keygen -v
     dnst nsec3-hash nlnetlabs.nl
     ldns-nsec3-hash nlnetlabs.nl
     ;;
 
   post-upgrade)
     # Nothing to do.
+    # Run some sanity checks
+    dnst --version
+    ldns-keygen -v
+    dnst nsec3-hash nlnetlabs.nl
+    ldns-nsec3-hash nlnetlabs.nl
     ;;
 esac
