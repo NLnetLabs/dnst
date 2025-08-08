1067ce971fb8baaca0d58e00bb6713521ca74747
Jannik Peters <jannik@nlnetlabs.nl>
2025-08-08 12:37:00 +0200

    Add tracing_subscriber to ldns test binary

--- src/bin/ldns.rs
+++ src/bin/ldns.rs
@@ -6,21 +6,37 @@
 
 use std::process::ExitCode;
 
+use dnst::env::Env;
+use dnst::log::LogFormatter;
 use dnst::try_ldns_compatibility;
+use tracing::level_filters::LevelFilter;
 
 fn main() -> ExitCode {
     let env = dnst::env::RealEnv;
+    run(env)
+}
 
-    let mut args = std::env::args_os();
-    args.next().unwrap();
+fn run(env: impl Env) -> ExitCode {
+    let mut args = env.args_os();
+    let argv0 = args.next().unwrap();
+    let stderr = env.stderr();
+    let subscriber = tracing_subscriber::FmtSubscriber::builder()
+        .with_ansi(stderr.is_terminal())
+        .with_writer(stderr)
+        .with_max_level(LevelFilter::WARN)
+        .event_format(LogFormatter {
+            program: argv0.to_string_lossy().to_string(),
+        });
+
     let args =
         try_ldns_compatibility(args).map(|args| args.expect("ldns commmand lacks ldns- prefix"));
-
-    match args.and_then(|args| args.execute(&env)) {
-        Ok(()) => ExitCode::SUCCESS,
-        Err(err) => {
-            err.pretty_print(env);
-            ExitCode::FAILURE
+    tracing::subscriber::with_default(subscriber.finish(), || {
+        match args.and_then(|args| args.execute(&env)) {
+            Ok(()) => ExitCode::SUCCESS,
+            Err(err) => {
+                err.pretty_print(env);
+                ExitCode::FAILURE
+            }
         }
-    }
+    })
 }
