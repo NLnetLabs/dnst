bd0e69bb96cd77cb4dc732beca1c9c0ea8ed79b3
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2025-10-03 12:58:22 +0200

    Try using cascade-dnst by its new name and path.

--- pkg/test-scripts/test-cascade-dnst.sh
+++ pkg/test-scripts/test-cascade-dnst.sh
@@ -6,11 +6,11 @@ set -x
 case $1 in
   post-install)
     # Run some sanity checks
-    dnst --version
+    /var/lib/cascade/bin/cascade-dnst --version
     # ldns-keygen -v
     # dnst nsec3-hash nlnetlabs.nl
     # ldns-nsec3-hash nlnetlabs.nl
-    dnst keyset --help
+    /var/lib/cascade/bin/cascade-dnst keyset --help
     man dnst
     # man dnst-keygen
     # man ldns-keygen
@@ -20,11 +20,11 @@ case $1 in
   post-upgrade)
     # Nothing to do.
     # Run some sanity checks
-    dnst --version
+    /var/lib/cascade/bin/cascade-dnst --version
     # ldns-keygen -v
     # dnst nsec3-hash nlnetlabs.nl
     # ldns-nsec3-hash nlnetlabs.nl
-    dnst keyset --help
+    /var/lib/cascade/bin/cascade-dnst keyset --help
     man dnst
     # man dnst
     # man dnst-keygen
