47e7de3e58a28a2ff348553b95d351ecedb2c58b
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2025-10-03 19:59:40 +0200

    Fix path to cascade-dnst.

--- pkg/test-scripts/test-cascade-dnst.sh
+++ pkg/test-scripts/test-cascade-dnst.sh
@@ -6,11 +6,11 @@ set -x
 case $1 in
   post-install)
     # Run some sanity checks
-    /var/lib/cascade/bin/cascade-dnst --version
+    /usr/libexec/cascade/cascade-dnst --version
     # ldns-keygen -v
     # dnst nsec3-hash nlnetlabs.nl
     # ldns-nsec3-hash nlnetlabs.nl
-    /var/lib/cascade/bin/cascade-dnst keyset --help
+    /usr/libexec/cascade/cascade-dnst keyset --help
     man cascade-dnst
     # man dnst-keygen
     # man ldns-keygen
@@ -20,11 +20,11 @@ case $1 in
   post-upgrade)
     # Nothing to do.
     # Run some sanity checks
-    /var/lib/cascade/bin/cascade-dnst --version
+    /usr/libexec/cascade/cascade-dnst --version
     # ldns-keygen -v
     # dnst nsec3-hash nlnetlabs.nl
     # ldns-nsec3-hash nlnetlabs.nl
-    /var/lib/cascade/bin/cascade-dnst keyset --help
+    /usr/libexec/cascade/cascade-dnst keyset --help
     man cascade-dnst
     # man dnst
     # man dnst-keygen
