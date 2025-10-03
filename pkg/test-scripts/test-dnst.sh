7108196ce4306744fd7c0dbcaf4b496a14871668
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2025-10-03 12:31:10 +0200

    Package as cascade-dnst instead of dnst.
    To avoid Cascade installing dnst and uninstalling ldns-utils that
    Cascade users may want to use such as ldns-verify-zone, which dnst
    doesn't provide yet. Also because this branch of dnst depends on an as
    yet unreleased version of the domain crate.

--- pkg/test-scripts/test-dnst.sh
+++ pkg/test-scripts/test-dnst.sh
@@ -7,23 +7,28 @@ case $1 in
   post-install)
     # Run some sanity checks
     dnst --version
-    ldns-keygen -v
-    dnst nsec3-hash nlnetlabs.nl
-    ldns-nsec3-hash nlnetlabs.nl
+    # ldns-keygen -v
+    # dnst nsec3-hash nlnetlabs.nl
+    # ldns-nsec3-hash nlnetlabs.nl
+    dnst keyset --help
     man dnst
-    man dnst-keygen
-    man ldns-keygen
+    # man dnst-keygen
+    # man ldns-keygen
+    man dnst-keyset
     ;;
 
   post-upgrade)
     # Nothing to do.
     # Run some sanity checks
     dnst --version
-    ldns-keygen -v
-    dnst nsec3-hash nlnetlabs.nl
-    ldns-nsec3-hash nlnetlabs.nl
+    # ldns-keygen -v
+    # dnst nsec3-hash nlnetlabs.nl
+    # ldns-nsec3-hash nlnetlabs.nl
+    dnst keyset --help
     man dnst
-    man dnst-keygen
-    man ldns-keygen
+    # man dnst
+    # man dnst-keygen
+    # man ldns-keygen
+    man dnst-keyset
     ;;
 esac
